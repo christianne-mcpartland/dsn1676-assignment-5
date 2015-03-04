5cfc5f6d17019f9c213e49b3386f226c538a93b0
Christiemcp <christianne.mcpartland@gmail.com>
2015-03-04 17:39:05 -0500

    Fixed Content.
    Clearly I didn’t commit enough. Very sorry!

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
 var $form = $('form');
+var $input = $('#the-items');
+var $theList = $('.list');
+var $li = $('<li>');
 
 $form.on('submit', function (eventObject) {
-
     eventObject.preventDefault();
     
     
-    var $theH2 = $('<h2>');
-    $theH2.html($input.val());
-    $theForm.prepend($theH2);
+    var $li = $('<li>');
+    $li.html($input.val());
     
+    var $inputRemove = $('<button class="remove xbutton">x</button>');
+    
+    $inputRemove.on('click', function () {
+        $li.remove('li');
+    });
+    
+    $li.click(function() {
+        $(this).addClass('done');
+    });
+    
+    $li.append($inputRemove);
+    $theList.append($li);
     $input.val('');
 });
